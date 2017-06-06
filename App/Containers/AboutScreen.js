94237d09d41272bdb81842e996498fadd8f1bbbe
Robin Heinze <robin.m.heinze@gmail.com>
2017-06-06 10:26:28 -0700

    Refactor info screen and add tabs

--- App/Containers/AboutScreen.js
+++ App/Containers/AboutScreen.js
@@ -27,6 +27,14 @@ class AboutScreen extends React.Component {
     )
   }
 
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      activeTab: 'liveHelp'
+    }
+  }
+
   tweetWithHashtag () {
     const appURL = 'twitter://post?hashtags=ChainReactConf'
     const webURL = 'https://twitter.com/intent/tweet?hashtags=ChainReactConf'
@@ -35,154 +43,171 @@ class AboutScreen extends React.Component {
     )
   }
 
-  render () {
+  setActiveTab (tab) {
+    this.setState({activeTab: tab})
+  }
+
+  renderSlack () {
     const gradient = ['#136EB5', 'rgba(1,192,182,0.88)']
+
+    return (
+      <LinearGradient
+        colors={gradient}
+        start={{x: 0, y: 1}}
+        end={{x: 1, y: 0}}
+        style={styles.slack}>
+        <Image style={styles.slackIcon} source={Images.slack} />
+        <Text style={styles.heading}>Join Us On Slack</Text>
+        <RoundedButton
+          text='Join the IR Slack Community'
+          onPress={() => Linking.openURL('http://community.infinite.red')}
+          style={styles.slackButton}
+        />
+      </LinearGradient>
+    )
+  }
+
+  renderWelcomeParty () {
+    return (
+      <View>
+        <Image source={Images.squarespacePhoto} style={{width: Metrics.screenWidth}}>
+          <View style={styles.afterPartyContainer}>
+            <View style={styles.partyHeader}>
+              <Image source={Images.sqspLogo} />
+              <Text style={styles.welcomeParty}>WELCOME PARTY</Text>
+              <Text style={styles.partyDescription}>AT SQUARESPACE PDX</Text>
+            </View>
+            <View style={styles.partyInfo}>
+              <Text style={styles.partyDescription}>SUNDAY, JULY 9 | 4-8PM</Text>
+              <Text style={styles.partyDescription}>311 SW WASHINGTON STREET</Text>
+            </View>
+          </View>
+        </Image>
+        <RoundedButton
+          onPress={() => Linking.openURL('https://chainreact.squarespace.com')}
+          style={styles.partyButton}
+        >
+          <Text style={styles.partyButtonText}>I WANT TO GO</Text>
+        </RoundedButton>
+      </View>
+    )
+  }
+
+  renderHashtag () {
+    return (
+      <View style={styles.twitter}>
+        <Image style={styles.blowhorn} source={Images.blowhorn} />
+        <TouchableOpacity onPress={() => this.tweetWithHashtag()}>
+          <Text style={styles.heading}>
+            #ChainReactConf
+          </Text>
+        </TouchableOpacity>
+        <Text style={styles.description}>
+          Make your friends jealous by tweeting, posting,
+          or whatever it is you do with the hashtag&nbsp;
+          <Text style={styles.hashtag} onPress={() => this.tweetWithHashtag()}>
+            #chainreactconf
+          </Text>.
+        </Text>
+      </View>
+    )
+  }
+
+  renderToggle () {
+    return (
+      <View>
+        <TouchableOpacity onPress={() => { this.setActiveTab('liveHelp') }}>
+          <Text>Live Help</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => { this.setActiveTab('sponsors') }}>
+          <Text>Sponsors</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
+  renderToggleContent () {
+    const { activeTab } = this.state
+
+    if (activeTab === 'liveHelp') {
+      return (this.renderLiveHelp())
+    } else {
+      return (this.renderSponsors())
+    }
+  }
+
+  renderLiveHelp () {
+    return (
+      <View>
+        <Text> Live Help </Text>
+      </View>
+    )
+  }
+
+  renderSponsor (url, image, style) {
+    return (
+      <TouchableOpacity
+        style={styles.sponsor}
+        onPress={() => Linking.openURL(url)}>
+        <Image style={style} source={image} />
+      </TouchableOpacity>
+    )
+  }
+
+  renderSponsors () {
+    return (
+      <View style={styles.sponsors}>
+        <Text style={styles.heading}>Our Sponsors</Text>
+        <Text style={styles.description}>
+          We love the sponsors we got for this conference.
+          They make all of this fun stuff possible, and we
+          couldn’t have done it without them.
+        </Text>
+
+        <Text style={styles.sponsorTierTitle}>Platinum Level</Text>
+        <View style={styles.sponsorTier}>
+          {this.renderSponsor('https://www.squarespace.com/', Images.squarespaceSponsor, styles.squarespaceSponsor)}
+        </View>
+
+        <Text style={styles.sponsorTierTitle}>Gold Level</Text>
+        <View style={styles.sponsorTier}>
+          {this.renderSponsor('https://nativebase.io/', Images.nativeBaseSponsor, styles.goldSponsor)}
+          {this.renderSponsor('https://formidable.com/', Images.formidableSponsor, styles.goldSponsor)}
+          {this.renderSponsor('https://moduscreate.com/', Images.modusSponsor, styles.goldSponsor)}
+        </View>
+
+        <Text style={styles.sponsorTierTitle}>Silver Level</Text>
+        <View style={styles.sponsorTier}>
+          {this.renderSponsor('https://aws.amazon.com/', Images.amazonSponsor)}
+          {this.renderSponsor('http://reactnative.training/', Images.trainingSponsor)}
+          {this.renderSponsor('https://gudog.co.uk/', Images.gudogSponsor)}
+          {this.renderSponsor('https://rangle.io/', Images.rangleSponsor)}
+        </View>
+
+        <Text style={styles.sponsorTierTitle}>Bronze Level</Text>
+        <View style={styles.sponsorTier}>
+          {this.renderSponsor('https://echobind.com/', Images.echobindSponsor)}
+          {this.renderSponsor('https://www.salesforce.com/', Images.salesforceSponsor)}
+          {this.renderSponsor('https://www.capitalone.com/', Images.capitalOneSponsor)}
+          {this.renderSponsor('https://www.instrument.com/', Images.instrumentSponsor)}
+          {this.renderSponsor('https://www.paypal.com/us/home', Images.paypalSponsor)}
+          {this.renderSponsor('http://www.qlik.com/us/', Images.qlikSponsor)}
+        </View>
+
+      </View>
+    )
+  }
+
+  render () {
     return (
       <PurpleGradient style={[styles.linearGradient, {flex: 1}]}>
         <ScrollView>
           <View style={styles.container}>
-            <LinearGradient
-              colors={gradient}
-              start={{x: 0, y: 1}}
-              end={{x: 1, y: 0}}
-              style={styles.slack}>
-              <Image style={styles.slackIcon} source={Images.slack} />
-              <Text style={styles.heading}>Join Us On Slack</Text>
-              <RoundedButton
-                text='Join the IR Slack Community'
-                onPress={() => Linking.openURL('http://community.infinite.red')}
-                style={styles.slackButton}
-              />
-            </LinearGradient>
-            <Image source={Images.squarespacePhoto} style={{width: Metrics.screenWidth}}>
-              <View style={styles.afterPartyContainer}>
-                <View style={styles.partyHeader}>
-                  <Image source={Images.sqspLogo} />
-                  <Text style={styles.welcomeParty}>WELCOME PARTY</Text>
-                  <Text style={styles.partyDescription}>AT SQUARESPACE PDX</Text>
-                </View>
-                <View style={styles.partyInfo}>
-                  <Text style={styles.partyDescription}>SUNDAY, JULY 9 | 4-8PM</Text>
-                  <Text style={styles.partyDescription}>311 SW WASHINGTON STREET</Text>
-                </View>
-              </View>
-            </Image>
-            <RoundedButton
-              onPress={() => Linking.openURL('https://chainreact.squarespace.com')}
-              style={styles.partyButton}
-            >
-              <Text style={styles.partyButtonText}>I WANT TO GO</Text>
-            </RoundedButton>
-            <View style={styles.twitter}>
-              <Image style={styles.blowhorn} source={Images.blowhorn} />
-              <TouchableOpacity onPress={() => this.tweetWithHashtag()}>
-                <Text style={styles.heading}>
-                  #ChainReactConf
-                </Text>
-              </TouchableOpacity>
-              <Text style={styles.description}>
-                Make your friends jealous by tweeting, posting,
-                or whatever it is you do with the hashtag&nbsp;
-                <Text style={styles.hashtag} onPress={() => this.tweetWithHashtag()}>
-                  #chainreactconf
-                </Text>.
-              </Text>
-            </View>
-            <View style={styles.sponsors}>
-              <Text style={styles.heading}>Our Sponsors</Text>
-              <Text style={styles.description}>
-                We love the sponsors we got for this conference.
-                They make all of this fun stuff possible, and we
-                couldn’t have done it without them.
-              </Text>
-
-              <Text style={styles.sponsorTierTitle}>Platinum Level</Text>
-              <View style={styles.sponsorTier}>
-                <TouchableOpacity
-                  style={styles.sponsor}
-                  onPress={() => Linking.openURL('https://www.squarespace.com/')}>
-                  <Image source={Images.squarespaceSponsor} />
-                </TouchableOpacity>
-              </View>
-
-              <Text style={styles.sponsorTierTitle}>Gold Level</Text>
-              <View style={styles.sponsorTier}>
-                <TouchableOpacity
-                  style={styles.sponsor}
-                  onPress={() => Linking.openURL('https://nativebase.io/')}>
-                  <Image style={styles.goldSponsor} source={Images.nativeBaseSponsor} />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={styles.sponsor}
-                  onPress={() => Linking.openURL('https://formidable.com/')}>
-                  <Image style={styles.goldSponsor} source={Images.formidableSponsor} />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={styles.sponsor}
-                  onPress={() => Linking.openURL('https://moduscreate.com/')}>
-                  <Image style={styles.goldSponsor} source={Images.modusSponsor} />
-                </TouchableOpacity>
-              </View>
-
-              <Text style={styles.sponsorTierTitle}>Silver Level</Text>
-              <View style={styles.sponsorTier}>
-                <TouchableOpacity
-                  style={styles.sponsor}
-                  onPress={() => Linking.openURL('https://aws.amazon.com/')}>
-                  <Image source={Images.amazonSponsor} />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={styles.sponsor}
-                  onPress={() => Linking.openURL('http://reactnative.training/')}>
-                  <Image source={Images.trainingSponsor} />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={styles.sponsor}
-                  onPress={() => Linking.openURL('https://gudog.co.uk/')}>
-                  <Image source={Images.gudogSponsor} />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={styles.sponsor}
-                  onPress={() => Linking.openURL('https://rangle.io/')}>
-                  <Image source={Images.rangleSponsor} />
-                </TouchableOpacity>
-              </View>
-
-              <Text style={styles.sponsorTierTitle}>Bronze Level</Text>
-              <View style={styles.sponsorTier}>
-                <TouchableOpacity
-                  style={styles.sponsor}
-                  onPress={() => Linking.openURL('https://echobind.com/')}>
-                  <Image source={Images.echobindSponsor} />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={styles.sponsor}
-                  onPress={() => Linking.openURL('https://www.salesforce.com/')}>
-                  <Image source={Images.salesforceSponsor} />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={styles.sponsor}
-                  onPress={() => Linking.openURL('https://www.capitalone.com/')}>
-                  <Image source={Images.capitalOneSponsor} />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={styles.sponsor}
-                  onPress={() => Linking.openURL('https://www.instrument.com/')}>
-                  <Image source={Images.instrumentSponsor} />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={styles.sponsor}
-                  onPress={() => Linking.openURL('https://www.paypal.com/us/home')}>
-                  <Image source={Images.paypalSponsor} />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={styles.sponsor}
-                  onPress={() => Linking.openURL('http://www.qlik.com/us/')}>
-                  <Image source={Images.qlikSponsor} />
-                </TouchableOpacity>
-              </View>
-
-            </View>
+            {this.renderSlack()}
+            {this.renderWelcomeParty()}
+            {this.renderHashtag()}
+            {this.renderToggle()}
+            {this.renderToggleContent()}
           </View>
         </ScrollView>
       </PurpleGradient>
