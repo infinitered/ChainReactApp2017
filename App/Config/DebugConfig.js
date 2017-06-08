41cd1df5f3168e86612bb1995fb2c60174dd6262
Gant Laborde <GantMan@gmail.com>
2017-06-08 12:25:31 -0500

    release 18

--- App/Config/DebugConfig.js
+++ App/Config/DebugConfig.js
@@ -4,6 +4,6 @@ export default {
   yellowBox: __DEV__,
   includeExamples: __DEV__,
   useReactotron: __DEV__,
-  hotwireDate: __DEV__,   // force today to be the day of the conf
+  hotwireDate: true, // __DEV__,   // force today to be the day of the conf
   hotwirePush: __DEV__    // force push notifications to happen in 5 seconds
 }
