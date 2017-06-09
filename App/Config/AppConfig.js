b7ed92bf9ce4e382a7790a38d02bfaf1d9b7d4b6
Gant Laborde <GantMan@gmail.com>
2017-06-09 13:24:42 -0500

    fix all the dates

--- App/Config/AppConfig.js
+++ App/Config/AppConfig.js
@@ -4,5 +4,7 @@ import '../I18n/I18n'
 
 export default {
   // font scaling override - RN default is on
-  allowTextFontScaling: false
+  allowTextFontScaling: false,
+  // Dates of the conference
+  conferenceDates: ['7/10/2017', '7/11/2017']
 }
