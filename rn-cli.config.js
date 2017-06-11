ddca75d12f837f802581d2dbf69fa976b375a005
Gant Laborde <GantMan@gmail.com>
2017-06-11 17:30:44 -0500

    converting several components

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  getTransformModulePath() {
+  getTransformModulePath () {
     return require.resolve('react-native-typescript-transformer')
   },
-  getSourceExts() {
-    return ['ts', 'tsx'];
+  getSourceExts () {
+    return ['ts', 'tsx']
   }
 }
