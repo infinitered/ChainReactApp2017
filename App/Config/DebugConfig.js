75cf7d741451e86864d639ff7955464a156ff66c
Gant Laborde <GantMan@gmail.com>
2017-06-17 16:00:02 -0500

    clean debug config finally

--- App/Config/DebugConfig.js
+++ App/Config/DebugConfig.js
@@ -3,7 +3,7 @@ export default {
   useFixtures: false,
   yellowBox: __DEV__,
   includeExamples: __DEV__,
-  useReactotron: true,
+  useReactotron: __DEV__,
   hotwireDate: true,  // __DEV__,   // force today to be the day of the conf
   hotwirePush: false  // force push notifications to happen in 5 seconds
 }
