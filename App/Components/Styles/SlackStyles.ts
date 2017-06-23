2ff3fb91744c0c479373dcb49e95c9202a6508f6
Ryan Linton <ryan@infinite.red>
2017-06-23 13:04:36 -0700

    Fix lint

--- App/Components/Styles/SlackStyles.ts
+++ App/Components/Styles/SlackStyles.ts
@@ -1,5 +1,5 @@
 import { StyleSheet } from 'react-native'
-import { ApplicationStyles, Colors, Fonts  } from '../../Themes/'
+import { ApplicationStyles, Colors, Fonts } from '../../Themes/'
 
 export default StyleSheet.create({
   ...ApplicationStyles.screen,
