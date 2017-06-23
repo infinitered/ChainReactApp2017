667dbd7f2aebba8d0d7a1a679451ac5d5a676a00
Gant Laborde <GantMan@gmail.com>
2017-06-23 15:37:45 -0500

    staging works

--- App/Config/DebugConfig.js
+++ App/Config/DebugConfig.js
@@ -4,7 +4,7 @@ export default {
   yellowBox: __DEV__,
   includeExamples: __DEV__,
   useReactotron: __DEV__,
-  codepushStaging: __DEV__,
-  hotwireDate: true,  // __DEV__,   // force today to be the day of the conf
+  codepushStaging: false,
+  hotwireDate: __DEV__,  // __DEV__,   // force today to be the day of the conf
   hotwirePush: __DEV__  // force push notifications to happen in 5 seconds
 }
