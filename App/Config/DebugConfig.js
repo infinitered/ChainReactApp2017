89a15ff417b146410c6929eb55ebc709e26d7027
Gant Laborde <GantMan@gmail.com>
2017-06-27 10:53:15 -0500

    kill API in prod

--- App/Config/DebugConfig.js
+++ App/Config/DebugConfig.js
@@ -7,5 +7,5 @@ export default {
   codepushStaging: false,
   hotwireDate: __DEV__,     // force today to be the day of the conf
   hotwirePush: __DEV__,     // force push notifications to happen in 5 seconds
-  getAPI: true              // should app hit API server for data?
+  getAPI: false             // should app hit API server for data? (Turn off if we're using codepush)
 }
